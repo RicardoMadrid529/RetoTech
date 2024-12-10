b7c5aeb933aad9b0f580b5811f223c23cc2809f6
RicardoMadrid529 <madridrichard19@gmail.com>
2024-12-09 20:24:43 -0500

    Update carrito.js

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             id: 1,
             nombre: 'Sombrero vueltiao con la bandera de Colombia, 23 vueltas',
-            precio: 250000,
+            precio: 254000,
             imagen: 'assets\img\Celular_Huawei_Nova_12_S_256GB_4G-2-removebg-preview.png',
             categoria: 'Celular_Huawei Nova 2S 256GB 4G'
         },
